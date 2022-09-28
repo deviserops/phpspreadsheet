3961317082619e77f1185305ddae0b289f475ce3
Hitesh Kumar <hgphpdev@gmail.com>
2022-09-28 17:59:17 +0530

    styling in sheet

--- src/PhpSpreadsheet/Sheet.php
+++ src/PhpSpreadsheet/Sheet.php
@@ -7,16 +7,17 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class Sheet {
 
-    public $spreadsheet;
-    public $sheet;
-    public $data;
+    private Spreadsheet $spreadsheet;
+    private \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet $sheet;
+    public array $data;
+    public string $name;
 
     public function __construct() {
         $this->spreadsheet = new Spreadsheet();
         $this->sheet = $this->spreadsheet->getActiveSheet();
     }
 
-    public function setCell() {
+    protected function setCell(): ?string {
         $row = 1;
         foreach ($this->data as $key => $valData) {
             $col = 'A';
@@ -31,7 +32,8 @@ class Sheet {
             }
             foreach ($valData as $content) {
                 if (is_array($content)) {
-                    $this->sheet->setCellValue($col . $row, $content['val']);
+                    $this->sheet->setCellValue($col . $row, $content['context']);
+                    $content['style'] ? $this->setStyle($col . $row, $content['style']) : null;
                 } else {
                     $this->sheet->setCellValue($col . $row, $content);
                 }
@@ -39,18 +41,50 @@ class Sheet {
             }
             $row++;
         }
+        return null;
     }
 
-    public function exportToExcel($data, $path = null) {
+    /**
+     * @param string $coordinate
+     * @param array $style
+     * @return object
+     */
+    protected function setStyle(string $coordinate, array $style): object {
+        $this->spreadsheet->getActiveSheet()->getStyle($coordinate)->applyFromArray($style);
+        return $this;
+    }
+
+    /**
+     * @param array $data This should be an multidimensional array
+     * @return $this
+     */
+    public function setArray(array $data): object {
         $this->data = $data;
         $this->setCell();
+        return $this;
+    }
 
+    /**
+     * @param string|null $name This should be without ext
+     * @return $this
+     */
+    public function setName(string $name = null): object {
+        $this->name = $name ?? uniqid();
+        return $this;
+    }
+
+    /**
+     * @param $path
+     * @return string|null
+     * @throws \PhpOffice\PhpSpreadsheet\Writer\Exception
+     * @example ../examples/export-excel.php
+     */
+    public function excel($path = null): ?string {
         $writer = new Xlsx($this->spreadsheet);
         $path = $path ? rtrim($path, '/') : null;
         $writer->save($path . '/' . uniqid() . '.xlsx');
+        return null;
     }
 
-    public function createExcel($data) {
-        $headers = ['h1', 'h2', 'h3', 'h4'];
-    }
+
 }
